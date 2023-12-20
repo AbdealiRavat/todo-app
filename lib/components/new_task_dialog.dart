3534554c35ec37ac8b2d5b4ad60b934b4bcda2ff
ankit-sysquo <ankit@sysquoinnovation.in>
2023-12-20 18:19:24 +0530

    new task dialog updated

--- lib/components/new_task_dialog.dart
+++ lib/components/new_task_dialog.dart
@@ -16,61 +16,49 @@ class NewTask extends StatelessWidget {
       elevation: 8,
       alignment: Alignment.center,
       backgroundColor: white,
-      contentPadding: const EdgeInsets.only(top: 10, right: 20, left: 20, bottom: 10),
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Icon((Icons.close)))
-        ],
-      ),
+      contentPadding: const EdgeInsets.only(top: 0, right: 20, left: 20, bottom: 10),
       shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
       actionsAlignment: MainAxisAlignment.center,
-      // actions: [
-      //   // TextButton(
-      //   //     onPressed: onCancel,
-      //   //     child: Text(
-      //   //       'CANCEL',
-      //   //       style: TextStyle(color: txt_green, letterSpacing: 2, fontSize: 18),
-      //   //     )),
-      //
-      // ],
-      content: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.7,
-        height: MediaQuery.of(context).size.height * 0.28,
-        child: Stack(
-          children: [
-            TextField(
-              controller: controller,
-              cursorOpacityAnimates: true,
-              // autofocus: true,
-              maxLines: 6,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(
-                        color: bg_green,
-                      )),
-                  hintText: 'Add New task',
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  hintStyle: TextStyle(color: green, fontSize: 16)),
-            ),
-            Positioned(
-              bottom: -5,
-              right: 0,
-              left: 0,
-              child: TextButton(
-                  onPressed: onSave,
-                  child: Text(
-                    'ADD',
-                    style: TextStyle(color: txt_green, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.w800),
-                  )),
-            ),
-          ],
-        ),
+      content: ListView(
+        shrinkWrap: true,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon((Icons.close)))
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: controller,
+            cursorOpacityAnimates: true,
+            // autofocus: true,
+            maxLines: 6,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: bg_green,
+                    )),
+                hintText: 'Add New task',
+                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                hintStyle: TextStyle(color: green, fontSize: 16)),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: onSave,
+                child: Text(
+                  'ADD',
+                  style: TextStyle(color: txt_green, letterSpacing: 2, fontSize: 20, fontWeight: FontWeight.w800),
+                )),
+          )
+        ],
       ),
     );
   }
